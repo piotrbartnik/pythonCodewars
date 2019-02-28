652e1606df0c289b4c84eb9b3dafc2dd202d7a1b
piotrbartnik <piotr.bartnik@hotmail.com>
2019-02-28 21:57:47 +0100

    python day 4

---
+++ 4.py
@@ -0,0 +1,16 @@
+#invert values
+
+def invert(lst):
+  i = 0
+  while i<len(lst):
+    if lst[i] < 0:
+      lst[i] = abs(lst[i])
+      i += 1
+    else:
+      lst[i] = -lst[i]
+      i += 1
+
+
+  print(lst)
+
+invert([1,-2,3,-4,5])
