6d03b5c4ca94511451fb12b3824a414fa519f16d
piotrbartnik <piotr.bartnik@hotmail.com>
2019-03-01 21:24:18 +0100

    fifth day python

---
+++ 5.py
@@ -0,0 +1,4 @@
+def find_needle(haystack):
+  return "found the needle at position " + str(haystack.index('needle'))
+
+find_needle(['3', '123124234', None, 'needle', 'world', 'hay', 2, '3', True, False])
