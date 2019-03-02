c75a2d4aa1d30b4c38da50e332a58396eca8e44f
piotrbartnik <piotr.bartnik@hotmail.com>
2019-03-02 11:31:09 +0100

    python day 6

---
+++ 6.py
@@ -0,0 +1,9 @@
+def number(bus_stops):
+  i = 0
+  people = 0
+  while i < len(bus_stops):
+    people += bus_stops[i][0]
+    people -= bus_stops[i][1]
+    i += 1
+  return people
+number([[3,0],[9,1],[4,10],[12,2],[6,1],[7,10]])
