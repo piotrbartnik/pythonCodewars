5222a754a035c95084dab045d985c335049fb8bc
piotrbartnik <piotr.bartnik@hotmail.com>
2019-03-03 11:25:21 +0100

    pythond day 7

---
+++ 7.py
@@ -0,0 +1,17 @@
+def nb_dig(n, d):
+  i = 0
+  res = []
+  while i<=n:
+    res.append(pow(i,2))
+    i += 1    
+  resString = "".join(str(e) for e in res)
+  j = 0 
+  counter = 0
+  while j<len(resString):    
+    if int(resString[j]) == d:
+      counter += 1
+    j += 1
+  return counter
+
+
+nb_dig(5750, 0)
