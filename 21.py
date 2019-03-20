716e8705788771edae35c0d5c01a334db932622f
piotrbartnik <piotr.bartnik@hotmail.com>
2019-03-20 22:31:28 +0100

    added python day21

---
+++ 21.py
@@ -0,0 +1,15 @@
+def tribonacci(signature, n): 
+  if n == 0:
+    return []
+  if n == 1:
+    return [signature[0]]
+  g = n
+  i = 0
+  while n > 0:
+    while len(signature) < g:
+      signature.append(sum(signature[slice(i,i+3)]))
+      i += 1
+    n -= 1
+  print(signature)
+
+tribonacci([1,1,1], 10)
