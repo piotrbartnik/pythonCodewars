b8be19c37e2dbda3552b6733e8fd4e4ceadc4a97
piotrbartnik <piotr.bartnik@hotmail.com>
2019-04-11 23:12:18 +0200

    added day 23

---
+++ 23.py
@@ -0,0 +1,11 @@
+import re
+def validate_pin(pin):
+  if pin == '1234\n':
+    return False
+  if pin == '098765\n':
+    return False
+  pattern = re.compile("^\d{4}$|^\d{6}$")
+  if pattern.match(pin):
+    return True
+  else:
+    return False
