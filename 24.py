0711c50e5a46b156e964e35c590aa5670bc162e2
piotrbartnik <piotr.bartnik@hotmail.com>
2019-04-15 22:14:41 +0200

    added day 24

---
+++ 24.py
@@ -0,0 +1,40 @@
+# code translator
+code = {
+  'A' : '@',
+  'B' : '8',
+  'C' : '(',
+  'D' : 'D',
+  'E' : '3',
+  'F' : 'F',
+  'G' : '6',
+  'H' : '#',
+  'I' : '!',
+  'J' : 'J',
+  'K' : 'K',
+  'L' : '1',
+  'M' : 'M',
+  'N' : 'N',
+  'O' : '0',
+  'P' : 'P',
+  'Q' : 'Q',
+  'R' : 'R',
+  'S' : '$',
+  'T' : '7',
+  'U' : 'U',
+  'V' : 'V',
+  'W' : 'W',
+  'X' : 'X',
+  'Y' : 'Y',
+  'Z' : '2',
+  ' ' : ' '
+}
+
+def to_leet_speak(str):
+  result = ''
+  for i in str:
+    result += code[i]
+  return result
+
+to_leet_speak('TEST')
+
+
