b7e82b308cca61e799f210c2fa04327718b0344f
Piotr Bartnik <piotr.bartnik@hotmail.com>
2019-10-05 13:21:24 +0200

    added day 30

--- 30.py
+++ 30.py
@@ -1,5 +1,13 @@
 def sort_array(ar):
-    print(ar.sort())
-
-
-sort_array([5, 3, 2, 8, 1, 4])
+  odd_list = []
+  for i in ar:
+    if i%2 != 0:
+      odd_list.append(i)
+  odd_list.sort()
+  i = 0
+  while i < len(ar):
+    if ar[i]%2 != 0:
+      ar[i] = odd_list[0]
+      odd_list.pop(0)
+    i += 1
+  return ar
