465af703d3f670e19d6e94391efa3d9fcefc7822
piotrbartnik <piotr.bartnik@hotmail.com>
2020-01-01 22:18:48 +0100

    added day 32

--- 32.py
+++ 32.py
@@ -1,2 +1,12 @@
-def squares_needed(grains):
-    # your code here
+def factorial(n):
+  if n == 0:
+    return 1
+  x = list(range(1, n+1))
+  sum = 1
+
+  for i in x:
+    sum *= i
+  return sum
+
+
+factorial(7)
