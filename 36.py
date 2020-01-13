152e7305811dc6148c4a9af31f03779c416eac56
piotrbartnik <piotr.bartnik@hotmail.com>
2020-01-13 21:29:43 +0100

    added final 36

--- 36.py
+++ 36.py
@@ -1,6 +1,7 @@
 def descending_order(num):
   res = list(str(num))
-  res.reverse()
+  res.sort()
+  res.reverse()  
   return int(''.join(res))
 
-descending_order(123456789)
+descending_order(124356789)
